34e8c628b62f8d0e0aa0a7922273be755e28dbde
rar9000 <clavius@Clavius.local>
2024-12-16 15:05:50 -0500

    experimented with index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,22 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+/*
+const sky = 'blue';
+const death = 'certain';
+const taxes = 'certain';
+
+let year = '2024';
+let dog = 'Chauncey';
+let gasTank = 'full';
+
+// sky = 'purple';
+year = '2025';
+
+console.log(sky);
+console.log(death);
+console.log(taxes);
+console.log(year);
+console.log(dog);
+console.log(gasTank);
+*/
